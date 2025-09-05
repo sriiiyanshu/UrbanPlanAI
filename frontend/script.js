6124e5e9d1bc24ce65da059efa5e7a2a2e2dd90a
Sriyanshu <sriiiyanshu@gmail.com>
2025-09-06 00:18:59 +0530

    location delhi

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ function initMap() {
   API_KEY = new URLSearchParams(scriptSrc.split("?")[1]).get("key");
 
   const mapOptions = {
-    center: { lat: 28.7041, lng: 77.1025 },
+    center: { lat: 28.6812, lng: 77.2223 },
     zoom: 12,
     mapTypeId: "satellite",
   };
